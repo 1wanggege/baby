d21e5e35ea11fbce24f662928f4b220756317581
1wanggege <1311127672@qq.com>
2019-01-10 18:59:42 +0800

    修复报错

---
+++ src/main/java/com/qf/entity/FoodPhotos.java
@@ -0,0 +1,63 @@
+package com.qf.entity;
+
+public class FoodPhotos {
+    private Integer id;
+
+    private String name;
+
+    private Integer fid;
+
+    private String path;
+
+    private Integer cid;
+
+    private Integer sid;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Integer getFid() {
+        return fid;
+    }
+
+    public void setFid(Integer fid) {
+        this.fid = fid;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path == null ? null : path.trim();
+    }
+
+    public Integer getCid() {
+        return cid;
+    }
+
+    public void setCid(Integer cid) {
+        this.cid = cid;
+    }
+
+    public Integer getSid() {
+        return sid;
+    }
+
+    public void setSid(Integer sid) {
+        this.sid = sid;
+    }
+}
